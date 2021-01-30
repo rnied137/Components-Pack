6a2ccc23b4768e1f9068005942162bea62eb0e55
Szelma <raduniek1@gmail.com>
2021-01-30 19:18:20 +0100

    Update Calendar.js

--- Calendar.js
+++ Calendar.js
@@ -58,14 +58,3 @@ export const Calendar = () => {
     </>
   );
 };
-
-// This will call a method named column, passing the rendered day’s index. We can then check to see if this is the first day, and if so, use moment to return the first day’s day of the week (0–6) plus one so it relates to our grid system. Add this method within a methods object on your Vue app.
-// column(index) {
-//   if (index == 0) {
-//     return this.days[0].day() + 1
-//   }
-//}
-
-// <div v-for="(day, index) in days" :style="{ gridColumn: column(index) }" :class="{ today: today(day) }">
-//   {{ day.format('D') }}
-// </div>
