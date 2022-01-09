767fc9fe891abb7d6f38d3a68394211a2cd366a3
Szelma <raduniek1@gmail.com>
2022-01-09 13:30:19 +0100

    Create Delivery.js

---
+++ Delivery.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { ReactComponent as DeliveryImg } from './delivery.svg'
+
+
+export const Delivery = () => {
+    return (
+       
+            <div style={{backgroundColor: "#7434a4", borderRadius: "15%",width: "326px", height: "326px", padding: "3rem"}}>
+      <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-between", width: "100%", height: "100%"}} >
+        <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "180px"}}>
+          <div style={{ borderRadius: "15px", backgroundColor: "#40D9B4", width:"29px", height:"13px"}}></div>
+          <div style={{ borderRadius: "15px", backgroundColor: "#40D9B4", width:"29px", height:"13px"}}></div>
+          <div style={{ borderRadius: "15px", backgroundColor: "#40D9B4", width:"29px", height:"13px"}}></div>
+          <div style={{ borderRadius: "15px", backgroundColor: "#fff", width:"64px", height:"13px"}}></div>
+        </div>
+          <DeliveryImg />
+          <div style={{position: "relative", bottom: "10px"}}>
+          <span style={{fontWeight: "550", fontSize: "3rem", color: "#fff", fontFamily: "DM Sans"}}> Out for Delivery</span>
+          </div>
+        </div>
+      </div>
+    )
+}
